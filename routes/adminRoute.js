42958a2bf748ed984d7625ee189dc5c41b67d752
seanolero <seanolero@outlook.com>
2024-10-16 16:53:52 +0300

    Add admin authentication middleware to admin routes

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const {checkAdmin, checkAdminVerification, createAdmin, sendAdminInvite, processAdminInvite, activateAdmin} = require("../controllers/adminController");
 const router = express.Router();
+const adminAuth = require('../middleware/adminAuth');
 
 router.route('/auth').post(checkAdmin)
 router.route('/auth/confirm-code').get(checkAdminVerification);
 
-router.route('/create').post(createAdmin);
-router.route('/invite/:id').post(sendAdminInvite).get(processAdminInvite);
+router.route('/create').post(adminAuth,createAdmin);
+router.route('/invite/:id').post(adminAuth,sendAdminInvite).get(processAdminInvite);
 router.route('/activate').post(activateAdmin);
 
 module.exports = router;
